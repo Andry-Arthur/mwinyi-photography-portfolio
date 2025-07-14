628ee5d87670a8439948d9578b633c601cbd405d
Tejaswi Amatya <tejuamatya.gmail.com>
2025-07-14 01:02:17 -0400

    Added the logo

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css'; // We'll create this CSS file next
-import logo from '../img/logo.png'
+import logo from '../img/logo.svg'
 
 function Navbar() {
   return (
